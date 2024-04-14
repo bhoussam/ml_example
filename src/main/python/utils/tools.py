a54639e431ebaa1e8e9658359794f6a6b74c6979
houssam <houssam.boulemia@yahoo.fr>
2024-04-14 21:23:10 +0200

    feat: add logger

--- src/main/python/utils/tools.py
+++ src/main/python/utils/tools.py
@@ -1,6 +1,8 @@
 import os
 
+import loguru
 from pyhocon import ConfigFactory
+from loguru import logger
 
 
 class Config(object):
@@ -11,3 +13,21 @@ class Config(object):
         if property_name not in self._config.keys():
             return None
         return self._config[property_name]
+
+
+def custom_logger(path: str, level: str = "INFO") -> loguru.logger:
+    """
+    A logger (from loguru) used to give some information to the user during the computation
+    in order to monitor the app
+    :param path: The log path folder
+    :param level: The log level which will be written in the log file
+    :return: A configured loguru logger that can be used and which will write in log file
+    """
+    logger.add(
+        sink="{}/ml.log".format(path),
+        level=level,
+        colorize=True,
+        rotation="1 week",
+        format="{time} {level} {message}"
+    )
+    return logger
